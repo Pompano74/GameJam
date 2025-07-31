0c5236ca7adca9c78f74c54ba134eeb458151325
Antoine <2325061@bdeb.qc.ca>
2025-07-31 14:39:05 -0400

    creation level 2 & 3

--- Level/Player/character_body_2d.gd
+++ Level/Player/character_body_2d.gd
@@ -3,9 +3,13 @@ extends CharacterBody2D
 
 var delta_time
 
+@export var ghost_node : PackedScene
+
 @onready var player_sprite_2d = $Sprite2D
 @onready var attack_object = $Attack
 @onready var attack_area_2d = $Attack/Attack_Sprite/Area2D
+@onready var ghost_timer = $GhostTimer
+@onready var particles = $GPUParticles2D
 
 var Dev_mode = false
 
@@ -14,7 +18,7 @@ var gravity_direction = Vector2.DOWN
 var gravity_force = 3
 var jump_direction = 1
 var player_speed = 300
-var player_jump_strength = -700
+var player_jump_strength = -850
 
 var dash_force = 1000
 var dash_direction = 1
@@ -52,9 +56,9 @@ func _process(delta):
 		
 func _physics_process(delta):
 	
+	#variables
 	var direction = Input.get_axis("move_left", "move_right")
 	attack_area_2d.monitoring = false
-	
 	#player direction
 	if Input.is_action_just_pressed("move_left"):
 			dash_direction = -1
@@ -64,63 +68,20 @@ func _physics_process(delta):
 		dash_direction = 1
 		player_sprite_2d.flip_h = false
 		attack_object.rotation = deg_to_rad(0)
-	
 	#free movement
 	if Dev_mode == true:
 			# change gravity direction
 		if Input.is_action_just_pressed("debug_1"):
-			if gravity_direction == Vector2.DOWN:
-				velocity.y = player_jump_strength * jump_direction
-				gravity_direction = Vector2.UP
-				jump_direction = -1
-				print("GRAVITY UP")
-			else:
-				velocity.y = player_jump_strength * jump_direction
-				gravity_direction = Vector2.DOWN
-				jump_direction = 1
-				print("GRAVITY DOWN")
-
-		
-		
+			gravity()
 		# dash
-		
 		if Input.is_action_just_pressed("dash"):
-			is_dashing = true
-			player_sprite_2d.modulate = Color(0,50,1,1)
-			dash_timer = dash_duration
-			velocity.x = dash_direction * dash_force
-			print("invicible")
-
-		# dash timer
-		if is_dashing:
-			dash_timer -= delta
-			if dash_timer <= 0:
-				is_dashing = false
-				player_sprite_2d.modulate = Color(1,1,1,1)
-				print("vulnerable")
-				if is_in_killzone == true:
-					die()
+			dash()
 		
-		#attack
 		if Input.is_action_just_pressed("attack"):
-			is_attack = true
-			attack_object.visible = true
-			attack_area_2d.monitoring = true
-			print("attacking")
-			attack_timer = attack_duration
-		
-		if is_attack:
-			attack_timer -= delta
-			if attack_timer <= 0:
-				is_attack = false
-				attack_object.visible = false
-				attack_area_2d.monitoring = false
-		
-
-		# Handle jump.
+			attack()
+				
 		if Input.is_action_just_pressed("jump"):
-			velocity.y = player_jump_strength * jump_direction
-	
+			jump()
 	#gravity
 	if gravity_direction == Vector2.DOWN:
 		if not is_on_floor():
@@ -130,7 +91,6 @@ func _physics_process(delta):
 		if not is_on_ceiling():
 			velocity += get_gravity() * gravity_direction * delta * gravity_force
 			player_sprite_2d.flip_v = true
-	
 	#side movement
 	if not is_dashing:
 		if direction:
@@ -139,17 +99,16 @@ func _physics_process(delta):
 			velocity.x = move_toward(velocity.x, 0, player_speed)
 	else:
 		velocity.x = lerp(velocity.x, direction * player_speed, 2 * delta)
-	
 	# dash timer
 	if is_dashing:
 		dash_timer -= delta_time
 		if dash_timer <= 0:
+			particles.emitting = false
 			is_dashing = false
 			player_sprite_2d.modulate = Color(1,1,1,1)
 			print("vulnerable")
 			if is_in_killzone == true:
 				die()
-	
 	#attack timer
 	if is_attack:
 		attack_timer -= delta_time
@@ -158,13 +117,16 @@ func _physics_process(delta):
 			attack_object.visible = false
 			attack_area_2d.monitoring = false		
 	move_and_slide()
+
 #Capacité joueur
 func jump():
 	velocity.y = player_jump_strength * jump_direction
 func dash():
+	particles.emitting = true
 	is_dashing = true
 	player_sprite_2d.modulate = Color(0,50,1,1)
 	dash_timer = dash_duration
+	velocity.y = -200 * jump_direction
 	velocity.x = dash_direction * dash_force
 	print("invicible")
 func attack():
@@ -184,17 +146,19 @@ func gravity():
 			gravity_direction = Vector2.DOWN
 			jump_direction = 1
 			print("GRAVITY DOWN")
+
+#attack kill
 func _on_area_2d_body_entered(body):
 	body.queue_free()
 	print("enemie killed")
 
+#player fail condition
 func on_killzone_enter():
 	print("body entered")
 	is_in_killzone = true
 func on_killzone_exit():
 	is_in_killzone = false
 	print("body exited")
-
 func die():
 	if is_dashing == false:
 		print("die func called")
