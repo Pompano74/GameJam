335601b821004f4508fae9758f113d9c79d0ed3f
Antoine <2325061@bdeb.qc.ca>
2025-08-01 13:12:10 -0400

    essaie de faire la turret
    epic fail

--- Level/Object/turret.gd
+++ Level/Object/turret.gd
@@ -1,18 +1,46 @@
 extends Node2D
 
 const BULLET = preload("res://Level/Object/bullet.tscn")
-@onready var pivot = $pivot
-var player: Node2D = null
 
+@onready var gun = $pivot
+@onready var fire_point = $pivot
+@onready var ray_cast_2d = $RayCast2D
+@onready var timer = $RayCast2D/Timer
+
+var player = null
 
 func _ready():
 	await get_tree().process_frame
-	player = get_tree().get_first_node_in_group("player")
-	if player == null:
-		print("⚠️ Joueur introuvable dans le groupe 'player'.")
-	
+	player = get_tree().get_nodes_in_group("player").front()
+	timer.start()
 
 func _process(delta):
-	if player and is_instance_valid(player):
-		pivot.look_at(player.global_position)
-		print("Joueur position:", player.global_position)
+	if player == null:
+		return
+
+	
+	var dir = global_position.direction_to(player.global_position)
+	var angle_to_target = dir.angle()
+	
+	
+	ray_cast_2d.rotation = angle_to_target
+	gun.rotation = angle_to_target
+
+	
+	if ray_cast_2d.is_colliding() and ray_cast_2d.get_collider().is_in_group("player"):
+		if timer.is_stopped():
+			shoot()
+
+func shoot():
+	print("pew")
+
+	var bullet = BULLET.instantiate()
+	get_tree().current_scene.add_child(bullet)
+	
+	bullet.global_position = fire_point.global_position
+	bullet.rotation = gun.rotation
+	
+	timer.start()
+
+func _on_timer_timeout():
+	pass
